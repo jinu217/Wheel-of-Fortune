c06c5fb6bfcc7d5d6633e01d3f34f8c5aa980737
qutiege-cloud <qutiege@gmail.com>
2026-08-18 16:23:15 +0900

    버튼 클릭, 플레이어 스탯

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    //플레이어 기본 능력치
+    [Header("Player Stats")]
+    public int health = 100;
+    public int attack = 20;
+    public int defense = 10;
+}
